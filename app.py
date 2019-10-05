ff3e945a33d1adc6cbdfc81329f6d1b267a55a50
siko408 <gamerbloxhd@gmail.com>
2019-10-05 11:25:08 -0700

    Adding user page

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from pymongo import MongoClient
-db = MongoClient()
+from bson.objectid import ObjectId
+
+client = MongoClient()
+db = client.contractor
+admin_list = db.admins  # Creating JSON obj
+users = db.users
+
 
 app = Flask(__name__)
 @app.route('/')
 def home_page():
-    return render_template('home_page.html')
+    for x in users.find():
+        print(x)
+    return render_template('welcome_page.html')
+
+
+@app.route('/login_page', methods=['GET'])
+def login_page():
+    return render_template('login_page.html', users=users.find())
+
+
+@app.route('/userpage', methods=['POST'])
+def logging_in():
+    print("Route post working")
+    #try:
+    #    user = users.find_one({'username': request.form.get('username')},
+                            #    {'password':request.form.get('password')})
+    #except Exception:
+    #    print("Exception")
+    #user = users.find()
+    return redirect(url_for('user_page'))
 
 
 if __name__ == '__main__':
